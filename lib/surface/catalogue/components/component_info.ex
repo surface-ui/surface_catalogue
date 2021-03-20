ff2e8705633ad4f94ec38dca4e21e3110db6507f
Malian De Ron <deronmalian@gmail.com>
2021-03-20 20:52:57 +0100

    Remove slots and events section from the heading doc

--- lib/surface/catalogue/components/component_info.ex
+++ lib/surface/catalogue/components/component_info.ex
@@ -67,7 +67,7 @@ defmodule Surface.Catalogue.Components.ComponentInfo do
     case Code.fetch_docs(module) do
       {:docs_v1, _, _, "text/markdown", %{"en" => doc}, _, _} ->
         parts =
-          String.split(doc, ["## Examples", "### Properties"])
+          String.split(doc, ["## Examples", "## Properties", "## Slots", "## Events"])
           |> List.first()
           |> String.split("\n\n", parts: 2)
 
