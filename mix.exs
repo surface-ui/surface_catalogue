149e7c5745359c700bfda697ae33fb208d52c8a7
Marlus Saraiva <marlus.saraiva@gmail.com>
2021-10-21 13:13:33 -0300

    Update surface to v0.6.0

--- mix.exs
+++ mix.exs
@@ -51,8 +51,7 @@ defmodule Surface.Catalogue.MixProject do
       {:html_entities, "~> 0.4"},
       {:plug_cowboy, "~> 2.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "~> 0.16.0"},
-      {:surface, github: "surface-ui/surface"},
+      {:surface, "~> 0.6.0"},
       {:earmark, "~> 1.3"},
       {:ex_doc, ">= 0.19.0", only: :docs},
       {:makeup_elixir, "~> 0.15.1"}
