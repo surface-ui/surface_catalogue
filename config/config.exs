5306d4fa995adcac6dec9f6335cbb8823bfe8338
Tiago Moraes <tiagoefmoraes@gmail.com>
2024-01-12 11:35:08 -0300

    Improve CI and fix warnings (#53)

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,9 @@
-import Mix.Config
+import Config
 
 config :phoenix, :json_library, Jason
 config :phoenix, :stacktrace_depth, 20
 
-config :logger, level: :warn
+config :logger, level: :warning
 config :logger, :console, format: "[$level] $message\n"
 
 # Configure esbuild (the version is required)
