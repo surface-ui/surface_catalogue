29660e7a386ed11149f6b9200c69f85e4bc798ef
Tiago Moraes <tiagoefmoraes@gmail.com>
2024-09-14 16:54:01 -0300

    Improve mix.exs, docs and hex page

--- mix.exs
+++ mix.exs
@@ -11,6 +11,8 @@ defmodule Surface.Catalogue.MixProject do
   use Mix.Project
 
   @version "0.6.2"
+  @source_url "https://github.com/surface-ui/surface_catalogue"
+  @homepage_url "https://surface-ui.org"
 
   def project do
     [
@@ -18,18 +20,24 @@ defmodule Surface.Catalogue.MixProject do
       version: @version,
       elixir: "~> 1.13",
       description: "An initial prototype of the Surface Catalogue",
+      start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers() ++ [:surface],
-      start_permanent: Mix.env() == :prod,
-      deps: deps(),
-      docs: docs(),
       aliases: aliases(),
       xref: [exclude: Surface.Catalogue.Playground],
+      deps: deps(),
+      preferred_cli_env: [docs: :docs],
+      # Docs
+      name: "Surface Catalogue",
+      source_url: @source_url,
+      homepage_url: @homepage_url,
+      docs: docs(),
       package: package()
     ]
     |> Blend.Premix.patch_project()
   end
 
+  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       mod: {Surface.Catalogue.Application, []},
@@ -37,12 +45,17 @@ defmodule Surface.Catalogue.MixProject do
     ]
   end
 
+  defp elixirc_paths(:dev), do: ["lib"] ++ catalogues()
+  defp elixirc_paths(:test), do: ["lib", "test/support"] ++ catalogues()
+  defp elixirc_paths(_), do: ["lib"]
+
   def catalogues do
     ["priv/catalogue"] ++ surface_catalogue_path()
   end
 
   defp surface_catalogue_path() do
-    Enum.find(deps(), &(elem(&1, 0) == :surface))
+    deps()
+    |> List.keyfind!(:surface, 0)
     |> surface_dep_opts()
     |> surface_catalogue_path()
   end
@@ -52,8 +65,8 @@ defmodule Surface.Catalogue.MixProject do
   defp surface_catalogue_path(opts) do
     path =
       case opts[:path] do
-        nil -> "deps/surface"
-        surface_dep_path -> Path.expand(surface_dep_path)
+        nil -> Mix.Project.deps_path() |> Path.relative() |> Path.join("/surface")
+        surface_dep_path -> Path.expand(surface_dep_path) |> IO.inspect(label: :expand)
       end
 
     ["#{path}/priv/catalogue"]
@@ -64,16 +77,13 @@ defmodule Surface.Catalogue.MixProject do
   defp surface_dep_opts({:surface, _req}), do: []
   defp surface_dep_opts(_), do: nil
 
-  defp elixirc_paths(:dev), do: ["lib"] ++ catalogues()
-  defp elixirc_paths(:test), do: ["lib", "test/support"] ++ catalogues()
-  defp elixirc_paths(_), do: ["lib"]
-
   defp aliases do
     [
       dev: "run --no-halt dev.exs"
     ]
   end
 
+  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
       {:surface, "~> 0.10"},
@@ -95,7 +105,6 @@ defmodule Surface.Catalogue.MixProject do
     [
       main: "readme",
       source_ref: "v#{@version}",
-      source_url: "https://github.com/surface-ui/surface_catalogue",
       nest_modules_by_prefix: [Surface.Catalogue],
       extras: [
         "README.md",
@@ -108,10 +117,20 @@ defmodule Surface.Catalogue.MixProject do
   defp package do
     %{
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/surface-ui/surface_catalogue"},
-      files:
-        ~w(assets lib priv/catalogue) ++
-          ~w(CHANGELOG.md LICENSE.md mix.exs README.md)
+      links: %{
+        Website: @homepage_url,
+        Changelog: "https://hexdocs.pm/surface_catalogue/changelog.html",
+        GitHub: @source_url
+      },
+      files: ~w(
+          README.md
+          CHANGELOG.md
+          LICENSE.md
+          mix.exs
+          lib
+          assets
+          priv/catalogue
+        )
     }
   end
 end
